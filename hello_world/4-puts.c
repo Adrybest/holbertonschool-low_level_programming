c6908edc7580e247a6380dd5e1c9398a237a8905
Adrybest <adryanbestard@gmail.com>
2024-02-19 13:39:26 +0000

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-fputs "Programming is like building a multilingual puzzle\n
+/**
+ * main - entry point
+ * return: Always 0 (sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
