c93f60696f72b4eadcd918b7114c7aec830050e2
Adrybest <adryanbestard@gmail.com>
2024-02-20 13:24:07 +0000

    Task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 
 	if ((n % 10) > 5)
 	{
-		printf("Last digit of %d and is greater than 5\n, n", n % 10);
+		printf("Last digit of %d and is greater than 5\n, n", digit);
 	}
 	else if ((n % 10) < 6)
 	{
