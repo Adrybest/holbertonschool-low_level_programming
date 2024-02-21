a4bf442dfb5d87c232a43db405d182b9d9396c1e
Adrybest <adryanbestard@gmail.com>
2024-02-21 09:33:27 +0000

    Task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print tebahpla
+ * Return: 0
+ */
+int main(void)
+{
+	char tebahpla;
+
+	for (tebahpla = 'z'; tebahpla <= 'a'; tebahpla++)
+		putchar(tebahpla);
+
+	putchar('\n');
+
+	return (0);
+}
