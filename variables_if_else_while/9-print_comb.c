f866feb03c3c45d3f5f4298f96b5183eee0cc6a4
Adrybest <adryanbestard@gmail.com>
2024-02-21 14:32:20 +0000

    Task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,11 +8,14 @@ int main(void)
 	int number = 0;
 
 	for (number = '0'; number <= '9'; number++)
+	{
+		putchar (number + '0');
 		if (number < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 
 	putchar('\n');
 	return (0);
