14718ae31753af6bbe4808a069bffe1e4a4c508e
Adrybest <adryanbestard@gmail.com>
2024-02-21 09:12:47 +0000

    Task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print number without used char
+ * Return: 0
+ */
+int main(void)
+{
+	while (number <= 9)
+	{
+		for (number = '0'; number = '9'; number++)
+			putchar(number);
+	}
+
+	putchar('\n');
+	return (0);
+}
