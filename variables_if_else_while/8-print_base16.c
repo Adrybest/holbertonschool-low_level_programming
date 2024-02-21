b289c1eb390adbc1524a245cb8c7c9a7f6fddec7
Adrybest <adryanbestard@gmail.com>
2024-02-21 13:32:52 +0000

    Task 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -6,15 +6,13 @@
 int main(void)
 {
 	int number;
+	char alphabet
 
 	for (number = "48"; number <= "57"; number++)
 		putchar(number);
-	{
-		char * alphabet;
 
 		for (alphabet = "97"; alphabet <= "102"; alphabet++)
 			putchar(alphabet);
-	}
 
 	putchar('\n');
 	return (0);
