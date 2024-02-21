fa47d6cd06896a16918c2fdf6dd892d287a68802
Adrybest <adryanbestard@gmail.com>
2024-02-21 08:31:12 +0000

    Task 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,9 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		putchar(alphabet);
 
 	if (alphabet != 'q' && alphabet != 'e')
+		putchar(alphabet);
 
 		putchar('\n');
 
