b168e82df442b763aa1a455f699eb8fd26a62606
Adrybest <adryanbestard@gmail.com>
2024-02-22 13:31:04 +0000

    Task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,9 +3,16 @@
  * main - print function
  * Return: 0
  */
-int main(void)
+int _putchar(char a)
 {
-	printf("_putchar\n");
+	printf('_');
+	printf('p');
+	printf('u');
+	printf('t');
+	printf('c');
+	printf('h');
+	printf('a');
+	printf('r');
 
 return (0);
 }
