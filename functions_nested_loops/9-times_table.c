15105276a602f0b514848c83dd1bc004a393ab27
Adrybest <adryanbestard@gmail.com>
2024-02-25 21:39:32 +0000

    Task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,10 +2,39 @@
 /**
  * times_table - print the table
  * Return: 0
+ *@n0, n1, n2, n3, n4: int
  */
 void times_table(void)
 {
-	int n0, n1;
-	
-	for (n0 = 0; n0 = 9; n0++)
-		_putchar("%d * %d = %d", 
+	int n0, n1, n2, n3, n4;
+
+	for (n0 = 0; n0 <= 9; n0++)
+	{
+		for (n1 = 0; n1 <= 9; n1++)
+		{
+			n2 = n0 * n1;
+
+			if (n2 > 9)
+			{
+				n3 = n2 % 10;
+				n4 = (n2 - n3) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(n4 + '0');
+				_putchar(n3 + '0');
+			}
+			else
+			{
+				if (n1 != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(n2 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
