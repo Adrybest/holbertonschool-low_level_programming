3c8ed7c15448e084e77e2df06e2b348a05a72179
Adrybest <adryanbestard@gmail.com>
2024-02-26 16:58:05 +0000

    Task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,7 @@ void more_numbers(void)
 		for (number = 0; number <= 14; number++)
 			_putchar(number / 10 + '0');
 			_putchar(number % 10 + '0');
-
-		_putchar('\n');
+			else
+				_putchar('\n');
 	}
 }
