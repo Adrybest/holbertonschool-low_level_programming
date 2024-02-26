4b68ac3fe1a446726fe3f1fde72770b438f32968
Adrybest <adryanbestard@gmail.com>
2024-02-26 14:01:51 +0000

    Task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_most_numbers - print the numbers w/out 2 and 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++ | n != 2 && n != 4)
+		_putchar(n);
+
+	_putchar('\n');
+}
