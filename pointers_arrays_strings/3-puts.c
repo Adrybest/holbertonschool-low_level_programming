aff3f6de65e88dbce87c5df36d8ef8dde410e2e3
Adrybest <adryanbestard@gmail.com>
2024-02-29 11:18:51 +0000

    Task 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,11 +1,15 @@
 #include "main.h"
 /**
- * _puts - prints a string
- * Return: 0
- * @str: int
- */
+* _puts - prints a string
+* @str: int
+*/
 void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; *str 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
