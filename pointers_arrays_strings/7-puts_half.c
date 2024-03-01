476848215dcfdd69a07c5355d89703a813073382
Adrybest <adryanbestard@gmail.com>
2024-03-01 11:25:39 +0000

    Task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,16 +7,16 @@ void puts_half(char *str)
 {
 	int a, b;
 
-	for (b = 1; str[b] != '\0'; b++)
-		
-		if (b % 2 == 1)
-		{
-			a = (b - 1) / 2;
-		}
-		else
-		{
-			a = b / 2;
-		}
+	for (b = 0; str[b] != '\0'; b++)
+
+	if (b % 2 == 1)
+	{
+		a = (b - 1) / 2;
+	}
+	else
+	{
+		a = b / 2;
+	}
 	a += 1;
 	for (; a < b; a++)
 	{
