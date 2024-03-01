a8040be0537ff2bd6d8ccb86b07a28aad8461b20
Adrybest <adryanbestard@gmail.com>
2024-03-01 11:12:00 +0000

    Task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - print half of a string
+ * @str: int
+ */
+void puts_half(char *str)
+{
+	int a, b;
+
+	for (b = 0; str[b] != '\0'; b++)
+		
+		if (b % 2 == 1)
+		{
+			a = (b - 1) / 2;
+		}
+		else
+		{
+			a = b / 2;
+		}
+			a += 1;
+		for (; a < b; a++)
+		{
+			_putchar(str[a]);
+		}
+	_putchar('\n');
+}
