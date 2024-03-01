ff89af59f44283ff560122c595f8516815f58b93
Adrybest <adryanbestard@gmail.com>
2024-03-01 08:40:15 +0000

    Task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - print reverses a string
+ * @s: int
+ */
+void rev_string(char *s)
+{
+	int a, b, c;
+
+	char temp;
+
+	{
+		for (a = 0; s[a] != '\0'; a++)
+		{
+		}
+
+		c = a -1;
+
+		for (b = 0; b < a / 2; b++)
+		{
+			temp = s[b];
+			s[b] = s[c];
+			s[c] = temp;
+			c--;
+		}
+	}
+}
