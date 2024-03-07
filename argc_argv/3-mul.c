1018f3765ade5759ca8053b825290d70969d2488
Adrybest <adryanbestard@gmail.com>
2024-03-07 22:25:51 +0000

    Task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that multiplies two numbers
+ * @argc: int
+ * @argv: tableau
+ * Return: Always (success)
+ */
+int main(int argc, char *argv[])
+{
+	int a = 0, b = 0, c;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a * b;
+	
+	printf("%d\n", c);
+	return (0);
+}
