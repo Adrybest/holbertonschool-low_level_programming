f997d7e71a8ae2aca3fdaea0a01bcded88b769fb
Adrybest <adryanbestard@gmail.com>
2024-03-11 11:02:02 +0000

    all

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * create_array - function that creates an array of chars.
+ * @size: int
+ * @c: char
+ * Return: Always (success)
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size = 0)
+	{
+		return ('\0');
+	}
+	if (
