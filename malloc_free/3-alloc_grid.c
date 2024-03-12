b9712796623bb2520469f4d5cbe43c84bb6829a9
Adrybest <adryanbestard@gmail.com>
2024-03-12 10:44:18 +0000

    Task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -15,10 +15,30 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	ptr = malloc(width * sizeof(int));
+	ptr = malloc(height * sizeof(int *));
 
-	if (ptr = '\0')
+	if (ptr == NULL)
 	{
-		return ('\0');
+		return (NULL);
+	}
+	for (a = 0; a < height; a++)
+	{
+		ptr[a] = malloc(width * sizeof(int));
+
+		if (ptr[a] == NULL)
+		{
+			for (; a >= 0; a--)
+			free(ptr[a]);
+
+			free(ptr);
+			return (NULL);
+		}
 	}
 
+	for (a = 0; a < height; a++)
+	{
+		for (b = 0; b < width; b++)
+			ptr[a][b] = 0;
+	}
+	return (ptr);
+}
