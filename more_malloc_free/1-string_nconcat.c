bd48d945b62c9ce7b85732b45ef111ba4de219cb
Adrybest <adryanbestard@gmail.com>
2024-03-13 09:56:15 +0000

    Task 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * string_nconcat - function that concatenates two strings
+ * @s1: ptr
+ * @s2: ptr
+ * @n: unsigned int
+ * Return: Always (success)
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int c = 0, a;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (a = 0; s1[a]; a++)
+	{
+		c++;
+	}
+	ptr = malloc(sizeof(char) * (c + 1));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	c = 0;
+
+	for (a = 0; s1[a]; a++)
+	{
+		ptr[c++] = s1[a];
+	}
+	for (a = 0; s2[a] && a < n; a++)
+	{
+		ptr[c++] = s2[a];
+	}
+	ptr[c] = '\0';
+	return (ptr);
+}
