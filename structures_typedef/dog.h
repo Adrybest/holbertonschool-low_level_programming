d88ffcb7967d1b454bc1edeefbf0d50ccd120402
Adrybest <adryanbestard@gmail.com>
2024-03-15 09:20:46 +0000

    Task dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,7 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * struct dog - define a new type of struct with elements
+ * @name: char
+ * @age: float
+ * @owner: char
+ */
 struct dog
 {
 	char *name;
