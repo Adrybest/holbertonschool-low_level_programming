c87408c33b7b54e8ce65df0e2517e0ca16af2c86
Adrybest <adryanbestard@gmail.com>
2024-03-15 20:10:02 +0000

    Task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,24 +1,81 @@
 #include "dog.h"
+
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *ptr);
+char *copy(char *a, char *b);
 /**
- * new_dog - creates a new dog
+ *new_dog - creates a new dog
  * @name: char
  * @age: float
  * @owner: char
+ * Return: Always (success)
  */
-dog_t *new_dog(char *name, float age, char *owner);
-	int _strlen(char *str);
-	char *_strcopy(char *d, char *src);
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *e;
+
+	if (name == NULL || age < 0 || owner == NULL)
+	{
+		return (NULL);
+	}
+	e = malloc(sizeof(dog_t));
+	if (e == NULL)
+	{
+		return (NULL);
+	}
+
+	e->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (e->name == NULL)
+	{
+		free(e);
+		return (NULL);
+	}
+
+	e->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (e->owner == NULL)
+	{
+		free(e->name);
+		free(e);
+		return (NULL);
+	}
+
+	e->name = copy(e->name, name);
+	e->age = age;
+	e->owner = copy(e->owner, owner);
+
+	return (e);
+}
+/**
+ * _strlen - calcul la longueur d'une chaine de caractere
+ * @ptr: compte
+ * Return: Always (success)
+ */
+
+int _strlen(char *ptr)
+{
+	int c;
+
+	while (*ptr++)
+	{
+		c++;
+	}
+	return (c);
+}
 
 /**
- * _strlen - calcule la longueur d'une chaîne done
- * @str: ptr
+ * copy - pour copier la chaine de caractere
+ * @a: char ptr
+ * @b: char ptr
+ * Return: Always (success)
  */
-int _strlen(char *str)
+char *copy(char *a, char *b)
 {
-	int len = 0;
+	int d;
 
-	while (*str++)
-		len++;
-
-	return (len);
+	for (d = 0; b[d]; d++)
+	{
+		a[d] = b[d];
+	}
+	a[d] = '\0';
+	return (a);
 }
