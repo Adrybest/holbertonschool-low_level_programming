ee57489c6a428a007416589970ad7d5461171b3f
Adrybest <adryanbestard@gmail.com>
2024-03-15 11:28:05 +0000

    Task 4 hard

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,21 +1,24 @@
 #include "dog.h"
 /**
  * new_dog - creates a new dog
- * strlen - calcule la longueur d'une chaîne donn�
- * @ptr: ptr
  * @name: char
  * @age: float
  * @owner: char
  */
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	int strlen(char *ptr)
-	{
-		int a;
+dog_t *new_dog(char *name, float age, char *owner);
+	int _strlen(char *str);
+	char *_strcopy(char *d, char *src);
 
-		for (a = 0; ptr[a]; a++)
-		{
-		}
-		return(ptr)
-	}
+/**
+ * _strlen - calcule la longueur d'une chaîne done
+ * @str: ptr
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
 }
