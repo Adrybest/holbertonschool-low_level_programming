700725f3a837df5cfda87d2663e02af017e2811a
Adrybest <adryanbestard@gmail.com>
2024-03-20 12:08:39 +0000

    Task 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,14 +16,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (com = 0; com < n; com++)
 	{
 		printf("%d", va_arg(liste, int));
+		if (com != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
 	}
-	if (com != (n - 1) && separator != NULL)
-	{
-		printf("%s", separator);
-	}
-	else
-	{
-		printf("\n");
-	}
+	printf("\n");
+	
 	va_end(liste);
 }
