44626eb8bd5ff9d1b6f8bf4b12e08d4cc135954c
Adrybest <adryanbestard@gmail.com>
2024-04-02 14:55:21 +0200

    Free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - function that free a list_t list
+ * @head: head
+ * Return: free
+*/
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+	free_list(head->next);
+	free(head->str);
+	free(head);
+}
