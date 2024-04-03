545b699bca540d90f5160b84861176cf2bb7f535
Adrybest <adryanbestard@gmail.com>
2024-04-03 11:21:44 +0200

    list length

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - print list
+ * @h: head
+ * Return:  the number of elements in a linked dlistint_t list
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		h = h->next;
+	}
+	return (i);
+}
