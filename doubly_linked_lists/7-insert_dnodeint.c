5d451039b4101c719d9ff9ad3fbda28b852f7777
Adrybest <adryanbestard@gmail.com>
2024-04-04 14:50:44 +0200

    creation of the file

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer
+ * @idx: int
+ * @n: int
+ * Return: the address of the new node
+*/
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t i;
+}
