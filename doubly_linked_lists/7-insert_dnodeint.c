9b89ec88c1db92116f233fac3a6812b9db88cdf2
Adrybest <adryanbestard@gmail.com>
2024-04-05 11:43:15 +0200

    insert at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (i->next == NULL)
 	{
-		return (add_dnodeint(h, n));
+		return (add_dnodeint_end(h, n));
 	}
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
